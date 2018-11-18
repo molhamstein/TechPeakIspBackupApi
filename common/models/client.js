5a1694541d56d8d835faa0fcb98c6172ce93547e
anas-1995 <anasalazmeh.95@gmail.com>
2018-11-18 17:34:28 +0200

    add to update

--- common/models/client.js
+++ common/models/client.js
@@ -8,11 +8,16 @@ var request = require('request');
 module.exports = function (Client) {
   let rawdata = fs.readFileSync('server/configISP.json');
   let dataInJson = JSON.parse(rawdata);
-  console.log(dataInJson);
 
 
   var accessToken = "";
   logIn();
+
+  function appendToFile(text) {
+    // fs.appendFile('log/log.txt', text + "\r\n", function (err) {
+    //   if (err) throw err;
+    // });
+  }
 
   function getData() {
     Client.find({
@@ -22,28 +27,31 @@ module.exports = function (Client) {
       var url;
       console.log(lastUser);
       if (lastUser[0] == null)
-        var url = dataInJson['host']+'api/clients/onlineUsersIsp?isExport=2&access_token=' + accessToken;
+        var url = dataInJson['host'] + 'api/clients/onlineUsersIsp?isExport=2&access_token=' + accessToken;
 
       else {
         var from = new Date(lastUser[0].update_at.toGMTString());
-        var url = dataInJson['host']+'api/clients/onlineUsersIsp?from=' + from.getUTCFullYear() + "-" + (from.getUTCMonth() + 1) + "-" + from.getUTCDate() + " " + from.getUTCHours() + ":" + from.getMinutes() + '&isExport=2&access_token=' + accessToken
+        var url = dataInJson['host'] + 'api/clients/onlineUsersIsp?from=' + from.getUTCFullYear() + "-" + (from.getUTCMonth() + 1) + "-" + from.getUTCDate() + " " + from.getUTCHours() + ":" + from.getMinutes() + ":" + from.getSeconds() + '&isExport=2&access_token=' + accessToken
       }
       console.log(url);
       request(url, function (error, response, body) {
         if (!error) {
           var mainBody = JSON.parse(body)
-          console.log("mainBody");
           console.log(mainBody);
-          for (var index = 0; index < mainBody.length; index++) {
-            var element = mainBody[index];
+          mainBody.forEach(function (element) {
+
             if (element.acctstoptime != null) {
+              appendToFile("client" + JSON.stringify(element))
+              appendToFile("go to update" + element.radacctid)
               update(element);
             } else {
+              appendToFile("go to add" + element.radacctid)
+
               add(element);
             }
-          }
+          }, this);
+
         } else {
-          console.log(error)
           logIn();
         }
       });
@@ -52,15 +60,13 @@ module.exports = function (Client) {
   // iFrZ3H7C29alZ5ORpK3gHnhXWXnUdEpryX8m0gUJqyPmgDDeFlln6GTTLdhzSZ9T
 
   function logIn() {
-    request.post(dataInJson['host']+'api/ISP/login', {
+    request.post(dataInJson['host'] + 'api/ISP/login', {
       json: dataInJson['loginData']
     }, (error, res, body) => {
       if (error) {
         console.error(error)
         return
       }
-      console.log(`statusCode: ${res.statusCode}`)
-      console.log(body)
       accessToken = body['id']
     })
   }
@@ -72,22 +78,37 @@ module.exports = function (Client) {
     getData();
   });
 
+  var timer = schedule.scheduleJob('30 * * * * *', function () {
+    getData();
+  });
+
+    var timer = schedule.scheduleJob('45 * * * * *', function () {
+    getData();
+  });
 
   function update(element) {
     Client.findOne({
       radacctid: element.radacctid
     }, function (err, oneClient) {
-      if (oneClient != null) {
-        oneClient.update_at = element.update_at;
-        oneClient.acctstoptime = element.acctstoptime;
+      if (err)
+        console.log(err);
+      else {
 
-        // console.log("Update")
-        // console.log("----------------------------")
-        // console.log(element.radacctid)
-        // console.log("----------------------------")
-        oneClient.save();
-      } else {
-        add(element);
+        if (oneClient!=null && oneClient[0] != null) {
+          oneClient.update_at = element.update_at;
+          oneClient.acctstoptime = element.acctstoptime;
+          oneClient.save();
+          appendToFile("update:" + oneClient.radacctid)
+        } else {
+          appendToFile("go to add from update" + element.radacctid)
+          Client.create(element, function (err, newClient) {
+            if (err)
+              console.log(err);
+            else
+            appendToFile("add from update:" + JSON.stringify(newClient))
+
+          })
+        }
       }
     })
   }
@@ -98,8 +119,13 @@ module.exports = function (Client) {
     }, function (err, oneClient) {
       if (oneClient == null) {
         Client.create(element, function (err, oneClient) {
-          //   console.log("added")
+          // console.log("added")
+          appendToFile("add:" + oneClient.radacctid)
+
         })
+      } else {
+        appendToFile("add is Field:" + oneClient.radacctid)
+
       }
     })
 
